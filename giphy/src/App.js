23b214e0e49c7a19612c239cca75b0e9b8ddb815
anupamdas104 <anupamdas104@gmail.com>
2019-06-06 10:44:06 -0400

    intial commit

---
+++ giphy/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchGiphy from "./components/SearchGiphy"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchGiphy></SearchGiphy>
+    </div>
+  );
+}
+
+export default App;
---
+++ giphy/src/components/SearchGiphy.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class SearchGiphy extends Component {
+  render(){
+  return (
+    <div className="App">
+      Hello SearchGihpy
+    </div>
+  );
+  }
+}
+
+export default SearchGiphy;
